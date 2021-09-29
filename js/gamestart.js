a0447f6dc6c4fe25d319ba151cbaa731ed3c6644
thiagoyasue <51247028+thiagoyasue@users.noreply.github.com>
2021-09-28 22:45:27 -0300

    adding the JS game

--- js/gamestart.js
+++ js/gamestart.js
@@ -1,29 +1,35 @@
-/*var canvas = document.createElement("canvas");
+var canvas = document.createElement("canvas");
 var contexto = canvas.getContext("2d");
 canvas.width = 500;
 canvas.height = 500;
+document.body.appendChild(canvas);
 
+canvas.style = "position:absolute; left: 50%; margin-left: -250px; top: 50%; margin-top: -200px; border-style: solid;";
 
 var bgReady = false;
 var bgImage = new Image();
 bgImage.onload = function(){
     bgReady = true;
 }
-bgImage.src = "img/background.png";
+bgImage.src = "img/game/background.png";
 
 var heroReady = false;
 var heroImage = new Image();
 heroImage.onload = function(){
     heroReady = true;
 }
-heroImage.src = "img/romarinho.png";
+
+function start(){
+    heroImage.src = "img/game/orion.png";
+}
+
 
 var monsterReady = false;
 var monsterImage = new Image();
 monsterImage.onload = function(){
     monsterReady = true;
 }
-monsterImage.src = "img/orion.png";
+monsterImage.src = "img/game/romarinho.png";
 
 var hero ={
     speed:256
@@ -47,8 +53,8 @@ var reset = function(){
     hero.x = canvas.width/2;
     hero.y = canvas.height/2;
 
-    monster.x = Math.random() * canvas.width;
-    monster.y = Math.random() * canvas.height;
+    monster.x = (Math.random() * (canvas.width -64));
+    monster.y = (Math.random() * (canvas.height - 64));
 };
 
 var update = function(modifier){
@@ -68,6 +74,12 @@ var update = function(modifier){
     {
         hero.x += hero.speed * modifier;
     }
+
+    if(hero.x <= (monster.x + 32) && monster.x <= (hero.x + 32) && hero.y <= (monster.y + 32) && monster.y <= (hero.y + 32))
+    {
+        ++monsterCapture;
+        capture();
+    }
 };
 
     var render = function(){
@@ -84,137 +96,52 @@ var update = function(modifier){
             contexto.drawImage(monsterImage, monster.x, monster.y);
         }
 
-        contexto.fillStyle = "rgb(0,0,0)";
+        contexto.fillStyle = "rgb(255,255,255)";
         contexto.font = "22px verdana";
         contexto.textAlign = "left";
         contexto.textBaseline = "top";
-        contexto.fillText("Monsters Captures: " + monsterCapture, 32, 32);
+        contexto.fillText("Hits: " + monsterCapture, 400, 450);
     };
 
     var main = function(){
         var now = Date.now();
-        var delta = now -TouchEvent;
+        var delta = now - then;
         update(delta /1000);
         render();
         then = now;
     };
 
+    function startTimer(duration, display) {
+        var timer = duration, seconds;
+
+        setInterval(function () {
+            seconds = parseInt(timer % 6, 10);
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+           
+            display.textContent = seconds;
+    
+            if (--timer <= 3) {
+                timer = duration;
+                capture();
+            }
+            
+        }, 1000);
+    }
+
+    var capture = function(){
+    
+        monster.x = (Math.random() * (canvas.width -64));
+        monster.y = (Math.random() * (canvas.height - 64));
+        timer = duration;
+    }
+    
+    window.onload = function () {
+        var fiveMinutes = 5,
+            display = document.querySelector('#time');
+        startTimer(fiveMinutes, display);
+    };
+    
+
     reset();
     var then = Date.now();
     setInterval(main,1);
-
-    document.body.appendChild(canvas);
-=============================
-*/
-
-   var canvas = document.createElement("canvas");
-   var contexto = canvas.getContext("2d");
-   canvas.width = 800;
-   canvas.height = 800;
-   document.body.appendChild(canvas);
-   
-   var bgReady = false;
-   var bgImage = new Image();
-   bgImage.onload = function(){
-       bgReady = true;
-   }
-   bgImage.src = "img/background.png";
-   
-   var heroReady = false;
-   var heroImage = new Image();
-   heroImage.onload = function(){
-       heroReady = true;
-   }
-   heroImage.src = "img/orion.png";
-   
-   var monsterReady = false;
-   var monsterImage = new Image();
-   monsterImage.onload = function(){
-       monsterReady = true;
-   }
-   monsterImage.src = "img/romarinho.png";
-   
-   var hero ={
-       speed:256
-   };
-   
-   var monster = {};
-   
-   var monsterCapture = 0;
-   
-   var teclaApertada = {};
-   
-   addEventListener("keydown", function(e){
-       teclaApertada[e.keyCode] = true;
-   },false);
-   
-   addEventListener("keyup", function(e){
-      delete teclaApertada[e.keyCode];
-   },false);
-   
-   var reset = function(){
-       hero.x = canvas.width/2;
-       hero.y = canvas.height/2;
-   
-       monster.x = 32 + (Math.random() * (canvas.width - 64));
-       monster.y = 32 + (Math.random() * (canvas.height - 64));
-   };
-   
-   var update = function(modifier){
-       if(38 in teclaApertada)
-       {
-           hero.y -= hero.speed * modifier;
-       }
-       if(40 in teclaApertada)
-       {
-           hero.y += hero.speed * modifier;
-       }
-       if(37 in teclaApertada)
-       {
-           hero.x -= hero.speed * modifier;
-       }
-       if(39 in teclaApertada)
-       {
-           hero.x += hero.speed * modifier;
-       }
-   
-       if(hero.x <= (monster.x + 32)&& monster.x <=(hero.x + 32) && hero.y <= (monster.y +32)
-       && monster.y <= (hero.y + 32)){
-           ++monsterCapture;
-           reset();
-       }
-   };
-   
-       var render = function(){
-           if(bgReady)
-           {
-               contexto.drawImage(bgImage, 0, 0);
-           }
-           if(heroReady)
-           {
-               contexto.drawImage(heroImage, hero.x, hero.y);
-           }
-           if(monsterReady)
-           {
-               contexto.drawImage(monsterImage, monster.x, monster.y);
-           }
-   
-           contexto.fillStyle = "rgb(0,0,0)";
-           contexto.font = "22px verdana";
-           contexto.textAlign = "left";
-           contexto.textBaseline = "top";
-           contexto.fillText("Monsters Captures: " + monsterCapture, 32, 32);
-       };
-   
-       var main = function(){
-           var now = Date.now();
-           var delta = now -then;
-           update(delta /1000);
-           render();
-           then = now;
-       };
-   
-       reset();
-       var then = Date.now();
-       setInterval(main,1);
-
