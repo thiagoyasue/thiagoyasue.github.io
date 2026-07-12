bd672eb84b7b1f1a1348b39075f7ff8a8f86d434
Thiago Yasue <thiagoyasue@proton.me>
2026-07-11 23:47:14 -0300

    changed index

---
+++ js/pessoas.js
@@ -0,0 +1,18 @@
+const pessoas = [
+    {
+        nome: "Andrevaz",
+        sexo: "Masculino",
+        cidade: "Mogi",
+        etnia: "Branco",
+        grupo: ["Designers", "Artistas"],
+        formacao: ["Design"]
+    },
+    {
+        nome: "Hiroshi",
+        geracao: "Geração Z",
+        cidade: "Mogi",
+        grupo: ["Mecatrônicos","Designers", "Artistas"],
+        ocupacao: ["Designer"]
+    },
+    
+];
